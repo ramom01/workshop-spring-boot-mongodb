bd78b7bbb57eb27e8b2af8336e4040daf324de1b
Ramon Rodrigues <rrsalescc@hotmail.com>
2020-02-18 15:04:28 -0300

    insercão de usuario com post

--- src/main/java/com/ramon/workshopmongo/services/UserService.java
+++ src/main/java/com/ramon/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ramon.workshopmongo.domain.User;
+import com.ramon.workshopmongo.dto.UserDTO;
 import com.ramon.workshopmongo.repository.UserRepository;
 import com.ramon.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = userRepository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
  }
